5ab894b4ff85b0be2415a422f666f456e5472661
Smk2kra <hakamgentakusuma@gmail.com>
2022-04-20 21:12:17 -0700

    first

---
+++ resources/views/_layout/_footer.blade.php
@@ -0,0 +1,63 @@
+<footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+          <a href="index.html" class="logo"><img src="assets/img/smkbisa.png" alt="" class="img-fluid"></a> 
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+            <li><i class="bx bx-chevron-right"></i> <a href="http://127.0.0.1:8000/">home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="http://127.0.0.1:8000/page/tentang">profil</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="http://127.0.0.1:8000/guru">guru</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="http://127.0.0.1:8000/berita">berita</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="http://127.0.0.1:8000/galeri">galeri</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="http://127.0.0.1:8000/kontak">kontak</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+            <li><i class="bx bx-chevron-right"></i> <a href="#">Help</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Support</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy Policy</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">term & Conditions</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Help & Support</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Contact Us</h4>
+            <p>Jl. Yos Sudarso, Jengglong, Bejen, Kec. Karanganyar, Kabupaten Karanganyar, Jawa Tengah 57716<br>
+              <strong>Phone:</strong> +62 345 6789 3301<br>
+              <strong>Email:</strong> karanganyar@gmails.com<br></p>
+            <div class="social-links mt-3">
+              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container py-4">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Website Sekolah</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/butterfly-free-bootstrap-theme/ -->
+      </div>
+    </div>
+  </footer>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,383 @@
+@extends('_layout.default')
+
+@section('content')
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>website sekolah</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Dosis:300,400,500,,600,700,700i|Lato:300,300i,400,400i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Butterfly - v4.7.0
+  * Template URL: https://bootstrapmade.com/butterfly-free-bootstrap-theme/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+ 
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+          <h2>WELCOME TO MY OUR SCHOOL</h2>
+          <h1>SMKN 2 KARANGANYAR</h1>
+          <h3>Mewujudkan pelajar yang berprestasi, berpendidikan karakter, memiliki sopan santun, dan lulusan siap kerja.</h3>
+          <div><a href="http://127.0.0.1:8000/kontak" class="btn-get-started scrollto">SEND MESSAGE</a></div>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img">
+          <img src="assets/img/smkbisa.png" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+   
+
+    <!-- ======= Counts Section ======= -->
+    <section id="counts" class="counts">
+      <div class="container">
+
+        <div class="text-center title">
+        <img src="assets/img/fotosmkn2.jpg" class="img-fluid" alt="">
+          <h3>Sejarah Sekolah</h3>
+          <text align:left/>
+          <p>Sekolah Menengah Kejuruan Negeri 2 Karanganyar atau yang sering disebut dengan SMK N 2 Karanganyar adalah sebuah Sekolah Menengah Kejuruan yang berlokasi di Jl. Laksda Yos Sudarso Telp. (0271) 494549 Fax. (0271) 6498171 Bejen, Karanganyar 57716.</p>
+          <p>SMK N 2 Karanganyar berdiri sejak tahun 1997 di area tanah seluas 27.720 m­­2 dan diresmikan pada tanggal 18 November 1997 oleh Menteri Pendidikan Nasional yaitu Prof. Dr. Ing. Wardiman Djojonegeoro dengan satu program studi Teknik Mesin.</P>
+          <p>Sekolah ini pertama kali dipimpin oleh Kepala Sekolah Drs. Surip Sunamto dari Tahun Pelajaran 1997/1998 hingga Tahun Pelajaran 2005/2006. Atas anjuran dari Bupati Karanganyar, pada Tahun Pelajaran 2004/2005 untuk membuka satu Progam Studi Teknologi Tekstil. Pada Tahun Pelajaran 2006/2007, karena belum ada Kepala Sekolah resmi maka Dinas Pendidikan Kabupaten Karanganyar menugaskan kepada Drs. Sugiyarso HS, S.Pd., S.H., M.Ag. sebagai pelaksana tugas Kepala Sekolah, barulah Tahun Pelajaran 2007/2008 dari seorang guru Karanganyar yang lulus ujian dan mendapatkan surat keputusan Bupati sebagai Kepala Sekolah yaitu Drs. Wahyu Widodo, M.T.</p>
+          <p>Ditahun Pelajaran 2008/2009, SMK N 2 Karanganyar membuka 2 Program Studi sekaligus, yaitu Teknik Otomotif Elektronik dan Rekayasa Perangkat Lunak.</p>
+        </div>
+
+
+        </div>
+
+      </div>
+    </section><!-- End Counts Section -->
+
+<!-- ======= Counts Section ======= -->
+<section id="counts" class="counts">
+      <div class="container">
+
+      
+
+        <div class="row counters position-relative">
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-purecounter-start="0" data-purecounter-end="15" data-purecounter-duration="1" class="purecounter"></span>
+            <p>Mata Pelajaran</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-purecounter-start="0" data-purecounter-end="110" data-purecounter-duration="1" class="purecounter"></span>
+            <p>Guru</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-purecounter-start="0" data-purecounter-end="40" data-purecounter-duration="1" class="purecounter"></span>
+            <p>Staff dan karyawan</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-purecounter-start="0" data-purecounter-end="1024" data-purecounter-duration="1" class="purecounter"></span>
+            <p>Mata pelajaran</p>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Counts Section -->
+
+
+    
+
+
+    <!-- ======= Testimonials Section ======= -->
+    <section id="testimonials" class="testimonials">
+      <div class="container position-relative">
+
+        <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
+          <div class="swiper-wrapper">
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <img src="assets/img/testimonials/soetanto.png" class="testimonial-img" alt="">
+                <h3>Drs. Soetanto, M.T</h3>
+                <h4>Kepala Sekolah</h4>
+                <p>
+                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                  Bismillahirohmannirrohim
+
+Assalamualaikum Warahmatullah Wabarakatuh
+
+
+Kami selaku pimpinan sekolah mengucapkan terima kasih kepada tim pembuat Website ini yang telah berusaha untuk dapat lebih memperkenalkan segala perihal yang dimiliki oleh sekolah. Dan tentunya Website sekolah kami masih terdapat banyak kekurangan, oleh karena itu kepada seluruh lapisan masyarakat dapat memberikan saran dan kritik yang membangun demi kemajuan Website ini lebih lanjut.
+
+Terima kasih sekian yang dapat kami sampaikan, apabila terdapat kekurangan dan kesalahan, mohon dimaafkan.
+
+Wassalamualaikum Warahmatullahi Wabarakatuh
+                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                </p>
+              </div>
+            </div><!-- End testimonial item -->
+
+            
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+
+      </div>
+    </section><!-- End Testimonials Section -->
+
+    <!-- ======= Team Section ======= -->
+    <section id="team" class="team section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>KOMPETENSI KEAHLIAN</h2>
+          <p>Pengertian kompetensi secara umum adalah suatu keahlian yang dimiliki oleh setiap individu dalam melakukan suatu tugas atau pekerjaan pada bidang tertentu, sesuai dengan jabatan yang sudah diberikan.</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member">
+              <div class="member-img">
+                <img src="assets/img/team/mesin.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Teknik Permesinan</h4>
+                <span>Mempelajari tentang cara memproduksi barang teknik dan menggunakan mesin.</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member">
+              <div class="member-img">
+                <img src="assets/img/team/kain.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Teknik Pembuatan Kain</h4>
+                <span>Jururan Teknik Pembuatan Kain Mempelajari tentang dasar pengetahuan bahan tekstil, proses dan pembuatan serat tekstil</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member">
+              <div class="member-img">
+                <img src="assets/img/team/oto.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Teknik Otrotonik</h4>
+                <span>Mempelajari tentang otomotif dalam penguasaan teknologi elektronik dan kontrol pada kendaraan.</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member">
+              <div class="member-img">
+                <img src="assets/img/team/rpl.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Rekayasa Perangkat Lunak</h4>
+                <span>Mempelajari tentang pengembangan perangkat lunak termasuk, pembuatan, pemeliharaan, dan manajemen organisasi.</span>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Team Section -->
+
+    <!-- ======= Gallery Section ======= -->
+    <section id="gallery" class="gallery">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>EKSTRAKURIKULER</h2>
+          <p>Kegiatan ekstrakurikuler adalah kegiatan di luar jam pelajaran sekolah, yang dilakukan di sekolah atau di luar sekolah dengan tujuan untuk menambah pengetahuan siswa, mengenai hubungan antar pelajaran, menyalurkan bakat dan minat, serta melengkapi pembinaan manusia seutuhnya.
+Kegiatan ini dilakukan secara berkala atau hanya dalam waktu-waktu tertentu dan ikut berpartisipasi (Yudha M. Saputra, 1998: 6).</p>
+        </div>
+
+        <div class="row no-gutters">
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="assets/img/gallery/osis1.jpg" class="galleery-lightbox" data-gallery="gallery-item">
+                <img src="assets/img/gallery/osis1.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="assets/img/gallery/pmi1.jpg" class="galleery-lightbox" data-gallery="gallery-item">
+                <img src="assets/img/gallery/pmi1.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="assets/img/gallery/paskibra.jpg" class="galleery-lightbox" data-gallery="gallery-item">
+                <img src="assets/img/gallery/paskibra.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="assets/img/gallery/pramuka.jpg" class="galleery-lightbox" data-gallery="gallery-item">
+                <img src="assets/img/gallery/pramuka.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="assets/img/gallery/rohis.jpg" class="galleery-lightbox" data-gallery="gallery-item">
+                <img src="assets/img/gallery/rohis.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="assets/img/gallery/jurnal.jpg" class="galleery-lightbox" data-gallery="gallery-item">
+                <img src="assets/img/gallery/jurnal.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="assets/img/gallery/imapa.jpg" class="galleery-lightbox" data-gallery="gallery-item">
+                <img src="assets/img/gallery/imapa.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Gallery Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+    <div class="container">
+    <div class="text-center mb-3">
+      <h1>Kontak Kami</h1>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="item-text">
+          <div class="item-text__title">Alamat</div>
+          <div class="item-text__main">
+            <p>Alamat lengkap sekolah</p>
+          </div>
+        </div>
+        <div class="item-text">
+          <div class="item-text__title">Email</div>
+          <div class="item-text__main">
+            <p>mail@website.com</p>
+          </div>
+        </div>
+        <div class="item-text">
+          <div class="item-text__title">Whatsapp</div>
+          <div class="item-text__main">
+            <p>+628xxxxxxx</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-8">
+
+        <h5>Kirim pesan</h5>
+
+        <div class="mb-3">Halo silahkan jika ada yang ditanyakan atau untuk mengirim kritik dan saran</div>
+
+        <div class="form-floating mb-3">
+          <input type="text" class="form-control" id="name" name="name" placeholder="Nama Lengkap">
+          <label for="name">Nama Lengkap</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input type="text" class="form-control" id="phone" name="phone" inputmode="tel"
+            placeholder="Nomor Telp/ WhatsApp">
+          <label for="phone">Nomor Telp/ WhatsApp</label>
+        </div>
+        <div class="form-floating mb-3">
+          <textarea class="form-control" placeholder="Isi Pesan" id="isi-pesan" style="height: 200px"></textarea>
+          <label for="isi-pesan">Isi Pesan</label>
+        </div>
+        <div class="mb-3">
+          <button type="submit" class="btn btn-primary">Kirim Pesan</button>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+
+  <div class="ratio ratio-16x9 mt-4" style="--bs-aspect-ratio: 300px;">
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15819.549735348359!2d110.9519918!3d-7.5872298!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe9c84052b95a73cb!2sPT.%20Kusuma%20Kreasi%20Utama!5e0!3m2!1sen!2sid!4v1644391270549!5m2!1sen!2sid"
+      width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+  </div>
+
+    
+    <!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
+@endsection
